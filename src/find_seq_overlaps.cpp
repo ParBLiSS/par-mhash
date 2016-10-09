c3cb0a304549a96688736b1b6dd986ec9110d29f
Sriram P C <sriram.pc@gmail.com>
2016-10-09 16:20:44 -0400

    Fix bug in mxx left shift for vector

--- src/find_seq_overlaps.cpp
+++ src/find_seq_overlaps.cpp
@@ -278,6 +278,42 @@ SeqMinHashGeneratorType = SeqMinHashGenerator<
 
 using BlockValueType = SeqMinHashGeneratorType<0>::value_type ;
 
+// specialization for std::vector
+template <typename T>
+std::vector<T> mxx_left_shift(const std::vector<T>& v, const mxx::comm& comm = mxx::comm())
+{
+    // get datatype
+    mxx::datatype dt = mxx::get_datatype<T>();
+
+    // TODO: handle tags with MXX (get unique tag function)
+    int tag = 15;
+    // receive the size first
+    std::vector<T> result;
+    size_t right_size = mxx::left_shift(v.size(), comm);
+
+    MPI_Request recv_req;
+    // if not last processor
+    // TODO: replace with comm.send/ comm.recv which automatically will resolve
+    // to BIG MPI if message size is too large
+    if (comm.rank() < comm.size()-1 && right_size > 0) {
+        result.resize(right_size);
+        MPI_Irecv(&result[0], right_size, dt.type(), comm.rank()+1, tag,
+                  comm, &recv_req);
+    }
+    // if not first processor
+    if (comm.rank() > 0 && v.size() > 0) {
+        // send my most right element to the right
+        MPI_Send(const_cast<T*>(&v[0]), v.size(), dt.type(), comm.rank()-1, tag, comm);
+    }
+    if (right_size > 0 && comm.rank() < comm.size()-1) {
+        // wait for the async receive to finish
+        MPI_Wait(&recv_req, MPI_STATUS_IGNORE);
+    }
+    return result;
+}
+
+
+
 void shiftStraddlingRegion(const mxx::comm& comm,
                            std::vector<BlockValueType>&  local_rhpairs,
                            std::size_t& start_offset,
@@ -303,7 +339,10 @@ void shiftStraddlingRegion(const mxx::comm& comm,
         snd_to_left.resize(osize);
         std::copy(local_rhpairs.begin(), fwx_itr, snd_to_left.begin());
     }
-    right_region = mxx::left_shift(snd_to_left, comm); // TODO: make sub-communicator ?
+    auto to_snd_size = snd_to_left.size();
+    auto to_rcv_size = mxx::right_shift(to_snd_size, comm);
+    std::cout << snd_to_left.size() << std::endl;
+    right_region = mxx_left_shift(snd_to_left, comm); // TODO: make sub-communicator ?
     start_offset = std::distance(local_rhpairs.begin(), fwx_itr);
 
     // find the ending segment of local_rhpairs that straddles
@@ -354,7 +393,6 @@ uint64_t generateOverlapReadPairs(const mxx::comm& comm,
     // shift straddling
     shiftStraddlingRegion(comm, local_rhpairs, start_offset, end_offset,
                           straddle_region);
-
     // generate pairs
     uint64_t csize,
         max_size = generatePairs(comm,
@@ -375,6 +413,7 @@ uint64_t generateOverlapReadPairs(const mxx::comm& comm,
         csize = generatePairs(comm, prev_rbv, rbv_itr, read_pairs);
 
     if(csize > max_size) max_size = csize;
+
    // remove duplicates
     comm.with_subset(read_pairs.begin() != read_pairs.end(), [&](const mxx::comm& comm){
         // sort read pairs
@@ -399,6 +438,7 @@ uint64_t generateOverlapReadPairs(const mxx::comm& comm,
           cur_itr++;
         }
       });
+    return max_size;
 }
 
 uint64_t load_file_data(mxx::comm& comm,
